69679a1f5dbc6adeb1ef6a52747a37838fb489a1
Anish <acharyaanish920@gmail.com>
2024-08-16 16:09:35 +0545

    offer secton

--- src/components/Offers.jsx
+++ src/components/Offers.jsx
@@ -1,20 +1,21 @@
 import quote from "../assets/offers/quote-icon.png";
+import { Offer } from "../util/offers";
 function Offers() {
   return (
-    <section className="py-10  lg:py-20  flex justify-center ">
-      <div className=" max-w-[1440px] px-6  flex-col w-full flex justify-between ">
-        <div className="flex flex-col md:flex-row justify-between md:gap-10 gap-4 ">
+    <section className="py-14  lg:py-24  flex justify-center ">
+      <div className=" max-w-[1440px] px-6 gap-20 flex-col w-full flex justify-between ">
+        <div className="flex flex-col md:flex-row justify-between md:gap-10 gap-6 ">
           <div className="py-4 flex  gap-4 flex-col">
             <h1 className="text-[30px] font-bold">
               Automated <br /> portfolio tracking
             </h1>
-            <p className="text-[18px] font-[400] md:w-[30rem]">
+            <p className="text-[18px] font-[400] md:w-[30rem] max-w-[30rem]">
               Yet bed any for travelling assistance indulgence unpleasing. Not
               thoughts all exercise blessing.
             </p>
           </div>
-          <div className="flex flex-col gap-4  bg-gray-200 p-4 md:w-[30rem] relative">
-            <figure className="absolute top-0">
+          <div className="flex flex-col gap-4  bg-gray-200 p-8   md:w-[30rem] relative">
+            <figure className="absolute top-0 -translate-y-5">
               <img src={quote} alt="" />
             </figure>
             <p>
@@ -25,10 +26,22 @@ function Offers() {
             <h2 className="font-bold">- Mike Taylor, Web Designer</h2>
           </div>
         </div>
-        <div>
-          <div>hello</div>
-          <div>hello</div>
-          <div>hello</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+          {Offer.map((item) => (
+            <div
+              key={item.id}
+              className="flex relative  flex-col justify-between gap-4 text-center md:pt-28 lg:pt-28 p-10 border-2 items-center border-gray-300 rounded-[1rem]"
+            >
+              <span className="absolute left-5 top-5 font-semibold text-[23px}">
+                {"0" + item.id}
+              </span>
+              <figure>
+                <img src={item.img} alt={item.id} />
+              </figure>
+              <h3 className="text-[23px] font-bold">{item.title}</h3>
+              <p className="opacity-70">{item.des}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/util/offers.js
@@ -0,0 +1,24 @@
+import offer1 from "../assets/offers/offer1.png";
+import offer2 from "../assets/offers/offer2.png";
+import offer3 from "../assets/offers/offer3.png";
+
+export const Offer = [
+  {
+    id: 1,
+    img: offer1,
+    title: " Connect wallets & companies",
+    des: "Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.",
+  },
+  {
+    id: 2,
+    img: offer2,
+    title: "Review transactions",
+    des: "Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.",
+  },
+  {
+    id: 3,
+    img: offer3,
+    title: "Get income insights.",
+    des: "Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.",
+  },
+];
